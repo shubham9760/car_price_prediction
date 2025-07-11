5a66b01b80d3cb8df7f51f9dba67021943d5beaa
Shubham Singh <30164142@adani.com>
2025-07-11 14:49:45 +0530

    Data Ingestion Pipeline made

--- src/car_price_prediction/components/data_ingestion.py
+++ src/car_price_prediction/components/data_ingestion.py
@@ -1,6 +1,6 @@
 import os
-from urllib.request import Request
 import zipfile
+from urllib.request import urlretrieve
 from car_price_prediction import logger
 from car_price_prediction.utils.common import get_size
 from car_price_prediction.entity.config_entity import DataIngestionConfig
@@ -12,21 +12,20 @@ class DataIngestion:
 
     def download_file(self):
         if not os.path.exists(self.config.local_data_file):
-            filename, headers = Request.urlretrieve(
-                url = self.config.source_URL,
-                filename = self.config.local_data_file
+            filename, headers = urlretrieve(
+                url=self.config.source_URL,
+                filename=self.config.local_data_file
             )
-            logger.info(f"{filename} download! with following info: \n{headers}")
+            logger.info(f"{filename} downloaded with the following info: \n{headers}")
         else:
-            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")
+            logger.info(f"File already exists with size: {get_size(Path(self.config.local_data_file))}")
 
     def extract_zip_file(self):
         """
-        zip_file_path: str
-        Extracts the zip file into the data directory
-        Function returns None
+        Extracts the zip file into the data directory.
         """
         unzip_path = self.config.unzip_dir
         os.makedirs(unzip_path, exist_ok=True)
         with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
             zip_ref.extractall(unzip_path)
+        logger.info(f"Extracted zip file to {unzip_path}")
